a3d1cd5262e6c9702734957b5bafb5839edac5df
lo den <disrute@gmail.com>
2024-05-18 13:00:07 +1000

    setup

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Hello world!\n";
+    string t = "greenus";
+    string green;
+    green = "text of greenus";
+    int sum;
+    sum = 5;
+    cout << t << "\n" << green << "\n" << sum << "\n";
+    return 0;
+}
